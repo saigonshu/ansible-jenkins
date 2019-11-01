6ed213aa1950bdf13ecea0225524ae6f00a1d093
saigang <saigangshu@gmail.com>
2019-11-01 17:27:56 +0800

    re-orginaze it as one playbook

--- install_with_playbook.sh
+++ install_with_playbook.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-mkdir -p ../tmp/roles/jenkins
-cp -r ./* ../tmp/roles/jenkins/
-cp -r ./molecule/inventories ../tmp/
-cp ./molecule/devops.yml ../tmp/
-cd ../tmp
-ansible-playbook -i inventories/hosts --limit aplus_182 devops.yml -vvv
-cd ..
+pipenv shell
 
-rm -rf ./tmp
+ansible-playbook -i inventories/hosts --limit aplus_182 playbook_devops.yml
